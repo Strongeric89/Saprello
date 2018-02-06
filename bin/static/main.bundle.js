48cd64097e983859c3482b9b242953393f6aa2dc
Strongeric89 <ericstrong89@gmail.com>
2018-02-06 16:21:40 +0000

    test2

--- bin/static/main.bundle.js
+++ bin/static/main.bundle.js
@@ -43,21 +43,10 @@ module.exports = "<!--The content below is only a placeholder and can be replace
 /***/ }),
 
 /***/ "../../../../../src/app/app.component.ts":
-<<<<<<< HEAD
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
-=======
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppComponent; });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__shared_post_post_service__ = __webpack_require__("../../../../../src/app/shared/post/post.service.ts");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_ng2_dragula_ng2_dragula__ = __webpack_require__("../../../../ng2-dragula/ng2-dragula.js");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_ng2_dragula_ng2_dragula___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2_ng2_dragula_ng2_dragula__);
->>>>>>> 7437356b4997924b50a55ae4b156656ac84250d8
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -67,16 +56,10 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-<<<<<<< HEAD
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = __webpack_require__("../../../core/esm5/core.js");
 var post_service_1 = __webpack_require__("../../../../../src/app/shared/post/post.service.ts");
 var ng2_dragula_1 = __webpack_require__("../../../../ng2-dragula/ng2-dragula.js");
-=======
-
-
-
->>>>>>> 7437356b4997924b50a55ae4b156656ac84250d8
 var AppComponent = /** @class */ (function () {
     function AppComponent(postService, dragulaService) {
         this.postService = postService;
@@ -94,58 +77,31 @@ var AppComponent = /** @class */ (function () {
         }, function (error) { return console.log(error); });
     };
     AppComponent = __decorate([
-<<<<<<< HEAD
         core_1.Component({
-=======
-        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Component"])({
->>>>>>> 7437356b4997924b50a55ae4b156656ac84250d8
             selector: 'app-root',
             template: __webpack_require__("../../../../../src/app/app.component.html"),
             styles: [__webpack_require__("../../../../../src/app/app.component.css")]
         }),
-<<<<<<< HEAD
         __metadata("design:paramtypes", [post_service_1.PostService, ng2_dragula_1.DragulaService])
     ], AppComponent);
     return AppComponent;
 }());
 exports.AppComponent = AppComponent;
-=======
-        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1__shared_post_post_service__["a" /* PostService */], __WEBPACK_IMPORTED_MODULE_2_ng2_dragula_ng2_dragula__["DragulaService"]])
-    ], AppComponent);
-    return AppComponent;
-}());
-
->>>>>>> 7437356b4997924b50a55ae4b156656ac84250d8
 
 
 /***/ }),
 
 /***/ "../../../../../src/app/app.module.ts":
-<<<<<<< HEAD
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
-=======
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__("../../../platform-browser/esm5/platform-browser.js");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_common_http__ = __webpack_require__("../../../common/esm5/http.js");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_ng2_dragula__ = __webpack_require__("../../../../ng2-dragula/index.js");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_ng2_dragula___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_3_ng2_dragula__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__app_component__ = __webpack_require__("../../../../../src/app/app.component.ts");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__shared_post_post_service__ = __webpack_require__("../../../../../src/app/shared/post/post.service.ts");
->>>>>>> 7437356b4997924b50a55ae4b156656ac84250d8
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-<<<<<<< HEAD
 Object.defineProperty(exports, "__esModule", { value: true });
 var platform_browser_1 = __webpack_require__("../../../platform-browser/esm5/platform-browser.js");
 var core_1 = __webpack_require__("../../../core/esm5/core.js");
@@ -153,19 +109,10 @@ var http_1 = __webpack_require__("../../../common/esm5/http.js");
 var ng2_dragula_1 = __webpack_require__("../../../../ng2-dragula/index.js");
 var app_component_1 = __webpack_require__("../../../../../src/app/app.component.ts");
 var post_service_1 = __webpack_require__("../../../../../src/app/shared/post/post.service.ts");
-=======
-
-
-
-
-
-
->>>>>>> 7437356b4997924b50a55ae4b156656ac84250d8
 var AppModule = /** @class */ (function () {
     function AppModule() {
     }
     AppModule = __decorate([
-<<<<<<< HEAD
         core_1.NgModule({
             declarations: [
                 app_component_1.AppComponent
@@ -177,46 +124,20 @@ var AppModule = /** @class */ (function () {
             ],
             providers: [post_service_1.PostService],
             bootstrap: [app_component_1.AppComponent]
-=======
-        Object(__WEBPACK_IMPORTED_MODULE_1__angular_core__["NgModule"])({
-            declarations: [
-                __WEBPACK_IMPORTED_MODULE_4__app_component__["a" /* AppComponent */]
-            ],
-            imports: [
-                __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */],
-                __WEBPACK_IMPORTED_MODULE_2__angular_common_http__["b" /* HttpClientModule */],
-                __WEBPACK_IMPORTED_MODULE_3_ng2_dragula__["DragulaModule"]
-            ],
-            providers: [__WEBPACK_IMPORTED_MODULE_5__shared_post_post_service__["a" /* PostService */]],
-            bootstrap: [__WEBPACK_IMPORTED_MODULE_4__app_component__["a" /* AppComponent */]]
->>>>>>> 7437356b4997924b50a55ae4b156656ac84250d8
         })
     ], AppModule);
     return AppModule;
 }());
-<<<<<<< HEAD
 exports.AppModule = AppModule;
-=======
-
->>>>>>> 7437356b4997924b50a55ae4b156656ac84250d8
 
 
 /***/ }),
 
 /***/ "../../../../../src/app/shared/post/post.service.ts":
-<<<<<<< HEAD
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
-=======
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return PostService; });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_common_http__ = __webpack_require__("../../../common/esm5/http.js");
->>>>>>> 7437356b4997924b50a55ae4b156656ac84250d8
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -226,14 +147,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-<<<<<<< HEAD
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = __webpack_require__("../../../core/esm5/core.js");
 var http_1 = __webpack_require__("../../../common/esm5/http.js");
-=======
-
-
->>>>>>> 7437356b4997924b50a55ae4b156656ac84250d8
 var PostService = /** @class */ (function () {
     function PostService(http) {
         this.http = http;
@@ -242,47 +158,27 @@ var PostService = /** @class */ (function () {
         return this.http.get('http://localhost:8080/endpoint');
     };
     PostService = __decorate([
-<<<<<<< HEAD
         core_1.Injectable(),
         __metadata("design:paramtypes", [http_1.HttpClient])
     ], PostService);
     return PostService;
 }());
 exports.PostService = PostService;
-=======
-        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Injectable"])(),
-        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1__angular_common_http__["a" /* HttpClient */]])
-    ], PostService);
-    return PostService;
-}());
-
->>>>>>> 7437356b4997924b50a55ae4b156656ac84250d8
 
 
 /***/ }),
 
 /***/ "../../../../../src/environments/environment.ts":
-<<<<<<< HEAD
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
-=======
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
->>>>>>> 7437356b4997924b50a55ae4b156656ac84250d8
 // The file contents for the current environment will overwrite these during build.
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
-<<<<<<< HEAD
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.environment = {
-=======
-var environment = {
->>>>>>> 7437356b4997924b50a55ae4b156656ac84250d8
     production: false
 };
 
@@ -290,7 +186,6 @@ var environment = {
 /***/ }),
 
 /***/ "../../../../../src/main.ts":
-<<<<<<< HEAD
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -304,24 +199,6 @@ if (environment_1.environment.production) {
     core_1.enableProdMode();
 }
 platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule)
-=======
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__ = __webpack_require__("../../../platform-browser-dynamic/esm5/platform-browser-dynamic.js");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__app_app_module__ = __webpack_require__("../../../../../src/app/app.module.ts");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__environments_environment__ = __webpack_require__("../../../../../src/environments/environment.ts");
-
-
-
-
-if (__WEBPACK_IMPORTED_MODULE_3__environments_environment__["a" /* environment */].production) {
-    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["enableProdMode"])();
-}
-Object(__WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__["a" /* platformBrowserDynamic */])().bootstrapModule(__WEBPACK_IMPORTED_MODULE_2__app_app_module__["a" /* AppModule */])
->>>>>>> 7437356b4997924b50a55ae4b156656ac84250d8
     .catch(function (err) { return console.log(err); });
 
 
